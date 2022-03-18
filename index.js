e413546d75f9782c9df84095ab1ecc78c9534c15
Jacob <jacobmjones17@gmail.com>
2022-03-18 16:03:25 -0400

    commit tree 07b23e787a99f49c1e334857ebb39997718f7c7c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+const userInput = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+const userObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(userInput),
+};
+
+function submitData() {
+    return fetch("http://localhost:3000/users", userObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            console.log(object.id);
+            document.querySelector("body").textContent = object.id
+        })
+        .catch(function (error) {
+            alert("Unauthorized Access");
+            console.log(error.message);
+            document.querySelector("body").textContent = error.message
+        });
+}
+
+
+
+
+
+// 1. Look at your console.log of the response
+// 2. Once you do that add that to the Dom
+// 3. Google: how to add an element to the dom
